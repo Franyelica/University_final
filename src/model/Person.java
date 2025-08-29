4d080f4a308c3c8a0799fa57364b04369e6806c0
Franyelica <franyelicavalentina.fvgf@gmail.com>
2025-08-29 10:44:27 -0500

    Clase Persona

---
+++ src/model/Person.java
@@ -0,0 +1,36 @@
+package model;
+
+public abstract class Person {
+    protected int id;
+    protected String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    protected abstract int generateId();
+}
